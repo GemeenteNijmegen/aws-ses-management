71dd3f6ed2593d33055735e96f39329d2d8621ab
Marnix Dessing <m.dessing@nijmegen.nl>
2023-12-11 16:09:57 +0100

    feat: deploy nijmegen.nl email domain in webforms-prod

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,4 +1,60 @@
+import { App } from 'aws-cdk-lib';
+import { SesMemberAccountStack } from '../src/SesManagementStage';
 
-test('Snapshot', () => {
-  console.log('No tests yet...');
+test('Configuration EmailIdentityConstruct', () => {
+  const app = new App();
+
+  // Should succeed ->
+  expect(() => new SesMemberAccountStack(app, 'test', {
+    configuration: {
+      accountEnvironment: {
+        account: '',
+        region: 'eu-west-1',
+      },
+      isAccountDomain: true,
+      name: 'test',
+    },
+  })).not.toThrow();
+
+  // Should throw error -> missing domain
+  expect(() => new SesMemberAccountStack(app, 'test2', {
+    configuration: {
+      accountEnvironment: {
+        account: '',
+        region: 'eu-west-1',
+      },
+      isAccountDomain: false,
+      emailDomain: undefined, // triggers errors
+      name: 'test',
+    },
+  })).toThrow();
+
+  // Should throw error -> missing mail from domain
+  expect(() => new SesMemberAccountStack(app, 'test3', {
+    configuration: {
+      accountEnvironment: {
+        account: '',
+        region: 'eu-west-1',
+      },
+      isAccountDomain: false,
+      emailDomain: 'csp-nijmegen.nl',
+      emailFromDomain: undefined, // triggers errors
+      name: 'test',
+    },
+  })).toThrow();
+
+  // Should succeed
+  expect(() => new SesMemberAccountStack(app, 'test4', {
+    configuration: {
+      accountEnvironment: {
+        account: '',
+        region: 'eu-west-1',
+      },
+      isAccountDomain: false,
+      emailDomain: 'csp-nijmegen.nl',
+      emailFromDomain: 'mail.csp-nijmegen.nl', // triggers errors
+      name: 'test',
+    },
+  })).not.toThrow();
+
 });
