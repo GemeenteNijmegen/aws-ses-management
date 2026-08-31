c2721a9055757c1952cdc6c4a380b02d21d08b18
Marnix Dessing <m.dessing@nijmegen.nl>
2026-08-31 13:50:15 +0200

    chore: fix build

---
+++ .projenrc.ts
@@ -0,0 +1,19 @@
+import { GemeenteNijmegenCdkApp } from '@gemeentenijmegen/projen-project-type';
+const project = new GemeenteNijmegenCdkApp({
+  cdkVersion: '2.1.0',
+  name: 'aws-ses-management',
+  repository: 'https://github.com/GemeenteNijmegen/aws-ses-management.git',
+  defaultReleaseBranch: 'main',
+  devDeps: [],
+  depsUpgradeOptions: {
+    workflowOptions: {
+      branches: ['main'], // No acceptance branch
+    },
+  },
+  projenrcTs: true,
+  deps: [
+    '@gemeentenijmegen/aws-constructs',
+    '@gemeentenijmegen/projen-project-type'
+  ],
+});
+project.synth();
